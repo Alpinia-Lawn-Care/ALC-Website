4e29566ddd2a6f6cbbbf75e178c7029aca676bb6
Ryan <ryanluna319@gmail.com>
2025-03-02 15:39:17 -0600

    Add FAQ and fix mobile site (add hamburger menu)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/sections/Hero';
+import FAQ from '../components/sections/FAQ';
 
 export default function Home() {
   return (
-    <>
+    <main>
       <Hero />
-    </>
+      <FAQ />
+    </main>
   );
 } 
